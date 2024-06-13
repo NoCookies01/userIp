8e2bdad092a8045eb61a6d04067fc9b0f92f27b2
AlexB <alex.bodnar315@gmail.com>
2024-06-13 21:48:14 +0300

    change ip getting

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = 3000
 
 const { getUserIp } = require('./getIp');
 
+app.set('trust proxy', true)
+
 app.get('/', (req, res) => {
-  var clientIp = getUserIp(req)
-  res.send(`Your Real IP Address is ${clientIp}.`)
+  //var clientIp = getUserIp(req)
+  res.send(`Your Real IP Address is ${req.ip}.`)
 })
 
 app.listen(port, () => {
